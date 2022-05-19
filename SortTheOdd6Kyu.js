f62e3389f378290f6101d4afa6dada8ac2f13472
timothy luedtke <timothyluedtke@gmail.com>
2022-05-19 11:00:56 -0500

    Added an incomplete FindTheMiddleElement7Kyu and Added SortTheOdd6Kyu

---
+++ SortTheOdd6Kyu.js
@@ -0,0 +1,42 @@
+//Sort The Odd 6Kyu
+
+// Task
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+// FUNDAMENTALS ARRAYS
+
+
+//take in array.
+// Return a sorted array.
+
+//remove odd numbers and order them.
+//compare evens and odds in array and replace odd numbers with ordered odd numbers.
+
+
+// function sortArray(array) {
+
+//     let oddArr = array.filter(e=>e%2 !== 0 ? e : false).sort((a,b)=>(a-b));
+//     let oddArrIndex = 0
+
+//       for(let i in array){
+//           if(array[i]%2 !== 0){
+//               array[i] = oddArr[oddArrIndex];
+//               oddArrIndex++;
+//             } 
+//       }
+//     return array
+//     }
+
+
+
+//Simplified
+
+
+function sortArray(array) {
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+  }
